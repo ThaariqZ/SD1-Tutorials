ebdbebda2013bfc7f295cba9b3cf45df35a0aaa7
ThaariqZ <145918518+ThaariqZ@users.noreply.github.com>
2023-10-25 09:35:28 +0530

    Create Q7.py

---
+++ Week 3/Q7.py
@@ -0,0 +1,12 @@
+import random
+chance = random.randint(0,1)
+
+if chance == 0 :
+    print(chance)
+    print("Heads")
+
+else:
+    print(chance)
+    print("Tails")
+
+# print("Heads" if chance==0 else "Tails")
